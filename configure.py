5c922b5808e97bdd4f1888ab04a0bbd6edb31e35
First Last <69217234+ieee802dot11ac@users.noreply.github.com>
2025-04-07 22:09:49 -0700

    oh lord (including dwarf shit)

--- configure.py
+++ configure.py
@@ -13,24 +13,17 @@
 ###
 
 import argparse
+import json
 import sys
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Union
+from tools.project import *
 
-from tools.project import (
-    Object,
-    ProgressCategory,
-    ProjectConfig,
-    calculate_progress,
-    generate_build,
-    is_windows,
+from tools.defines_common import (
+    cflags_includes,
+    DEFAULT_VERSION,
+    VERSIONS
 )
-
-# Game versions
-DEFAULT_VERSION = 0
-VERSIONS = [
-    "GAMEID",  # 0
-]
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -145,162 +138,165 @@ if not config.non_matching:
 config.binutils_tag = "2.42-1"
 config.compilers_tag = "20240706"
 config.dtk_tag = "v1.3.0"
-config.objdiff_tag = "v2.4.0"
+config.objdiff_tag = "v2.7.1"
 config.sjiswrap_tag = "v1.2.0"
-config.wibo_tag = "0.6.11"
+config.wibo_tag = "0.6.16"
 
 # Project
-config.config_path = Path("config") / config.version / "config.yml"
-config.check_sha_path = Path("config") / config.version / "build.sha1"
+config_dir = Path("config") / config.version
+config_json_path = config_dir / "config.json"
+objects_path = config_dir / "objects.json"
+config.config_path = config_dir / "config.yml"
+config.check_sha_path = config_dir / "build.sha1"
+config.reconfig_deps = [
+    config_json_path,
+    objects_path,
+]
+
+# SCRATCH_PRESETS = [
+#     114, # SZBE69 ("Rock Band 3")
+#     123, # SZBE69_B8 ("Rock Band 3 (Debug)")
+# ]
+# config.scratch_preset_id = SCRATCH_PRESETS[version_num]
+
+# Build flags
+flags = json.load(open(config_json_path, "r", encoding="utf-8"))
+progress_categories: dict[str, str] = flags["progress_categories"]
+asflags: list[str] = flags["asflags"]
+ldflags: list[str] = flags["ldflags"]
+cflags: dict[str, dict] = flags["cflags"]
+
+def get_cflags(name: str) -> list[str]:
+    return cflags[name]["flags"]
+def add_cflags(name: str, flags: list[str]):
+    cflags[name]["flags"] = [*flags, *cflags[name]["flags"]]
+
+def get_cflags_base(name: str) -> str:
+    return cflags[name].get("base", None)
+
+def are_cflags_inherited(name: str) -> bool:
+    return "inherited" in cflags[name]
+def set_cflags_inherited(name: str):
+    cflags[name]["inherited"] = True
+
+def apply_base_cflags(key: str):
+    if are_cflags_inherited(key):
+        return
+
+    base = get_cflags_base(key)
+    if base is None:
+        add_cflags(key, cflags_includes)
+    else:
+        apply_base_cflags(base)
+        add_cflags(key, get_cflags(base))
+
+    set_cflags_inherited(key)
+
+# Set up base flags
+base_cflags = get_cflags("base")
+base_cflags.append(f"-d BUILD_VERSION={version_num}")
+base_cflags.append(f"-d VERSION_{config.version}")
+
+# Set conditionally-added flags
+# cflags
+if args.debug:
+    base_cflags.append("-sym dwarf-2,full")
+    # Causes code generation memes, use only in desperation
+    # base_cflags.append("-pragma \"debuginline on\"")
+else:
+    base_cflags.append("-DNDEBUG=1")
+
+# ldflags
+if args.debug:
+    ldflags.append("-gdwarf-2")
+if config.generate_map:
+    ldflags.extend(["-mapunused", "-listclosure"])
+
+# Apply cflag inheritance
+for key in cflags.keys():
+    apply_base_cflags(key)
+
 config.asflags = [
-    "-mgekko",
-    "--strip-local-absolute",
-    "-I include",
-    f"-I build/{config.version}/include",
+    *asflags,
     f"--defsym BUILD_VERSION={version_num}",
     f"--defsym VERSION_{config.version}",
 ]
-config.ldflags = [
-    "-fp hardware",
-    "-nodefaults",
-]
-if args.debug:
-    config.ldflags.append("-g")  # Or -gdwarf-2 for Wii linkers
-if args.map:
-    config.ldflags.append("-mapunused")
-    # config.ldflags.append("-listclosure") # For Wii linkers
+config.ldflags = ldflags
 
-# Use for any additional files that should cause a re-configure when modified
-config.reconfig_deps = []
+config.linker_version = "Wii/1.3"
 
-# Optional numeric ID for decomp.me preset
-# Can be overridden in libraries or objects
-config.scratch_preset_id = None
+config.shift_jis = False
+config.progress_all = False
 
-# Base flags, common to most GC/Wii games.
-# Generally leave untouched, with overrides added below.
-cflags_base = [
-    "-nodefaults",
-    "-proc gekko",
-    "-align powerpc",
-    "-enum int",
-    "-fp hardware",
-    "-Cpp_exceptions off",
-    # "-W all",
-    "-O4,p",
-    "-inline auto",
-    '-pragma "cats off"',
-    '-pragma "warn_notinlined off"',
-    "-maxerrors 1",
-    "-nosyspath",
-    "-RTTI off",
-    "-fp_contract on",
-    "-str reuse",
-    "-multibyte",  # For Wii compilers, replace with `-enc SJIS`
-    "-i include",
-    f"-i build/{config.version}/include",
-    f"-DBUILD_VERSION={version_num}",
-    f"-DVERSION_{config.version}",
-]
-
-# Debug flags
-if args.debug:
-    # Or -sym dwarf-2 for Wii compilers
-    cflags_base.extend(["-sym on", "-DDEBUG=1"])
-else:
-    cflags_base.append("-DNDEBUG=1")
-
-# Metrowerks library flags
-cflags_runtime = [
-    *cflags_base,
-    "-use_lmw_stmw on",
-    "-str reuse,pool,readonly",
-    "-gccinc",
-    "-common off",
-    "-inline auto",
-]
-
-# REL flags
-cflags_rel = [
-    *cflags_base,
-    "-sdata 0",
-    "-sdata2 0",
-]
-
-config.linker_version = "GC/1.3.2"
-
-
-# Helper function for Dolphin libraries
-def DolphinLib(lib_name: str, objects: List[Object]) -> Dict[str, Any]:
-    return {
-        "lib": lib_name,
-        "mw_version": "GC/1.2.5n",
-        "cflags": cflags_base,
-        "progress_category": "sdk",
-        "objects": objects,
-    }
-
-
-# Helper function for REL script objects
-def Rel(lib_name: str, objects: List[Object]) -> Dict[str, Any]:
-    return {
-        "lib": lib_name,
-        "mw_version": "GC/1.3.2",
-        "cflags": cflags_rel,
-        "progress_category": "game",
-        "objects": objects,
-    }
-
-
-Matching = True                   # Object matches and should be linked
-NonMatching = False               # Object does not match and should not be linked
-Equivalent = config.non_matching  # Object should be linked when configured with --non-matching
-
-
-# Object is only matching for specific versions
-def MatchingFor(*versions):
-    return config.version in versions
-
+# Object files
+Matching = True
+Equivalent = config.non_matching
+NonMatching = False
 
 config.warn_missing_config = True
 config.warn_missing_source = False
-config.libs = [
-    {
-        "lib": "Runtime.PPCEABI.H",
-        "mw_version": config.linker_version,
-        "cflags": cflags_runtime,
-        "progress_category": "sdk",  # str | List[str]
-        "objects": [
-            Object(NonMatching, "Runtime.PPCEABI.H/global_destructor_chain.c"),
-            Object(NonMatching, "Runtime.PPCEABI.H/__init_cpp_exceptions.cpp"),
-        ],
-    },
-]
 
+def get_object_completed(status: str) -> bool:
+    if status == "MISSING":
+        return NonMatching
+    elif status == "Matching":
+        return Matching
+    elif status == "NonMatching":
+        return NonMatching
+    elif status == "Equivalent":
+        return Equivalent
+    elif status == "LinkIssues":
+        return NonMatching
 
-# Optional callback to adjust link order. This can be used to add, remove, or reorder objects.
-# This is called once per module, with the module ID and the current link order.
-#
-# For example, this adds "dummy.c" to the end of the DOL link order if configured with --non-matching.
-# "dummy.c" *must* be configured as a Matching (or Equivalent) object in order to be linked.
-def link_order_callback(module_id: int, objects: List[str]) -> List[str]:
-    # Don't modify the link order for matching builds
-    if not config.non_matching:
-        return objects
-    if module_id == 0:  # DOL
-        return objects + ["dummy.c"]
-    return objects
+    assert False, f"Invalid object status {status}"
 
-# Uncomment to enable the link order callback.
+libs: list[dict] = []
+objects: dict[str, dict] = json.load(open(objects_path, "r", encoding="utf-8"))
+for (lib, lib_config) in objects.items():
+    # config_cflags: str | list[str]
+    config_cflags: list[str] = lib_config.pop("cflags")
+    lib_cflags = get_cflags(config_cflags) if isinstance(config_cflags, str) else config_cflags
+
+    lib_objects: list[Object] = []
+    # config_objects: dict[str, str | dict]
+    config_objects: dict[str, Union[str, dict[str, Union[str, Any]]]] = lib_config.pop("objects")
+    if len(config_objects) < 1:
+        continue
+
+    for (path, obj_config) in config_objects.items():
+        if isinstance(obj_config, str):
+            completed = get_object_completed(obj_config)
+            lib_objects.append(Object(completed, path))
+        else:
+            completed = get_object_completed(obj_config["status"])
+
+            if "cflags" in obj_config:
+                object_cflags = obj_config["cflags"]
+                if isinstance(object_cflags, str):
+                    obj_config["cflags"] = get_cflags(object_cflags)
+
+            lib_objects.append(Object(completed, path, **obj_config))
+
+    libs.append({
+        "lib": lib,
+        "cflags": lib_cflags,
+        "host": False,
+        "objects": lib_objects,
+        **lib_config
+    })
+
+config.libs = libs
+
+# def link_order_callback(module_id: int, objects: List[str]) -> List[str]:
+#     # Don't modify the link order for matching builds
+#     if not config.non_matching:
+#         return objects
+#     return objects
+
 # config.link_order_callback = link_order_callback
 
-
-# Optional extra categories for progress tracking
-# Adjust as desired for your project
-config.progress_categories = [
-    ProgressCategory("game", "Game Code"),
-    ProgressCategory("sdk", "SDK Code"),
-]
+# Progress tracking categories
+config.progress_categories = [ProgressCategory(name, desc) for (name, desc) in progress_categories.items()]
 config.progress_each_module = args.verbose
 
 if args.mode == "configure":
